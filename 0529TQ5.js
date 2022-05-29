8e42cf604b9a59ce9913745d36bdb4a4cb7a9a7b
SkAmin185 <100791275+SkAmin185@users.noreply.github.com>
2022-05-29 20:45:01 +0530

    Add files via upload

---
+++ 0529TQ5.js
@@ -0,0 +1,24 @@
+let date = 5
+switch (date) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+  console.log(day);
---
+++ T1Q3.js
@@ -0,0 +1,8 @@
+let l = 10;
+let b = 20;
+if(l==b){
+    console.log("Its a square");
+}
+else{
+    console.log("Its a rectrangle")
+}
